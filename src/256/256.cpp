3a0f83375b79cfa7bb5fa990ceaee3ee94109574
Sai Cheemalapati <saicheems@gmail.com>
2013-07-16 00:00:53 +0000

    added uva 256

---
+++ src/256/256.cpp
@@ -0,0 +1,31 @@
+/*
+ * Sai Cheemalapati
+ * UVA 256: Quirksome squares
+ *
+ */
+
+#include<cmath>
+#include<cstdio>
+#include<iostream>
+#include<iomanip>
+#include<vector>
+
+using namespace std;
+
+int N;
+vector<int> squares;
+
+int prep() {
+	for(int i = 0; i < 10000; i++)
+		squares.push_back(i * i);
+}
+
+int main() {
+	prep();
+	while(scanf("%d", &N) == 1) {
+		for(int i = 0; i < squares.size(); i++) {
+			if(squares[i] >= (int) pow(10, N)) break;
+			if(pow(squares[i] % (int) pow(10, N / 2) + squares[i] / (int) pow(10, N / 2), 2) == squares[i]) cout << setw(N) << setfill('0') << squares[i] << endl;
+		}	
+	}
+}
