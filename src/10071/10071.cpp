02e7b5d980b9c40f7620eb01d6f368058917661f
Sai Cheemalapati <saicheems@gmail.com>
2013-08-15 23:01:05 -0400

    added uva 10071

---
+++ src/10071/10071.cpp
@@ -0,0 +1,16 @@
+/*
+ * Sai Cheemalapati
+ * UVA 10071: Back to high school physics
+ *
+ */
+
+#include<cstdio>
+
+using namespace std;
+
+int a, b;
+
+int main() {
+	while(scanf("%d %d", &a, &b) == 2)
+		printf("%d\n", a * b * 2);
+}
