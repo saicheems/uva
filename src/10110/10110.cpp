23a135a9d225b252b704409f913879effb858c6b
saicheemalapati <saicheemalapati@gmail.com>
2013-10-18 19:47:40 -0400

    added uva 10110

---
+++ src/10110/10110.cpp
@@ -0,0 +1,19 @@
+/*
+ * Sai Cheemalapati
+ * UVA 10110: Light, more light
+ *
+ */
+
+#include<cmath>
+#include<cstdio>
+
+long long a;
+
+int main() {
+	while(scanf("%lld", &a) == 1) {
+		if(a == 0) break;
+		long long root = sqrt(a);
+		printf("%s\n", (root * root) == a? "yes" : "no");
+	}
+}
+
