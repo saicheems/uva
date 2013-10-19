da49627ada526fc3bed844d6479935f2381607f8
saicheemalapati <saicheemalapati@gmail.com>
2013-10-18 22:56:37 -0400

    added uva 424

---
+++ src/424/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Sai Cheemalapati
+ * UVA 424: Integer inquiry
+ *
+ */
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+class Main {
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		BigInteger zero = BigInteger.valueOf(0);
+		BigInteger sum = BigInteger.valueOf(0);
+		for(;;) {
+			BigInteger b = s.nextBigInteger();
+			if(b.equals(zero)) break;
+			sum = sum.add(b);
+		}
+		System.out.println(sum.toString());
+	}
+}
