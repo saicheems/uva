ec605af8e047233b41b748347f32bb4fcdb063ff
saicheemalapati <saicheemalapati@gmail.com>
2014-03-08 18:56:56 -0500

    added uva 713

---
+++ src/713/Main.java
@@ -0,0 +1,27 @@
+/*
+ * UVa 713: Adding reversed numbers
+ * Sai Cheemalapati
+ *
+ */
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        for (int i = 0; i < N; i++) {
+            String a = new StringBuilder(scanner.next()).reverse().toString();
+            String b = new StringBuilder(scanner.next()).reverse().toString();
+            String c = new StringBuilder((new BigInteger(a).add(new BigInteger(b))).toString()).reverse().toString();
+
+            int offset = 0;
+            for (int j = 0; j < c.length() - 1; j++)
+                if(c.charAt(j) == '0') offset++;
+                else break;
+
+            System.out.println(c.substring(offset));
+        }
+    }
+}
