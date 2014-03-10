f2685d886d8500ed3e9febb6b2646403e1e8a610
saicheemalapati <saicheemalapati@gmail.com>
2014-03-10 16:19:09 -0400

    fixed if spacing

--- src/10062/10062.cpp
+++ src/10062/10062.cpp
@@ -23,8 +23,8 @@ int main() {
     char string[1024];
     for (int T = 0;; T++) {
         order.clear();
-        if(!gets(string)) break;
-        if(T > 0) printf("\n");
+        if (!gets(string)) break;
+        if (T > 0) printf("\n");
 
         for (int i = 0; i < 256; i++) freq[i] = 0;
         for (int i = 0; string[i] != '\0'; i++) {
@@ -32,7 +32,7 @@ int main() {
         }
 
         for (int i = 0; i < 256; i++) {
-            if(freq[i] != 0) order.push_back(make_pair(i, freq[i]));
+            if (freq[i] != 0) order.push_back(make_pair(i, freq[i]));
         }
 
         sort(order.begin(), order.end(), compare);
